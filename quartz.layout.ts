d636f1cb63cd06f8cdeb782623a2d7719d29fd7d
Cristian Araujo <araujocristian@live.com>
2025-03-20 17:55:20 -0300

    Quartz sync: Mar 20, 2025, 5:55 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/araujocristian",
+      Linkedin: "https://linkedin.com/in/araujocristian",
     },
   }),
 }
@@ -37,7 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
